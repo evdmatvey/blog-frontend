afdd2f2b6e3859e2229c86d565e7106b6c09d509
NyTakoeSebe <evd.matvey@mail.ru>
2023-07-04 14:23:11 +0700

    page loader bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
 import { Provider } from 'react-redux';
+import NProgress from 'nprogress';
 import { wrapper } from '@/app/redux/store';
+import { Layout } from '@/widgets/Layout';
 
 import '../app/styles/global.scss';
-import { Layout } from '@/widgets/Layout';
+import '../app/styles/nprogress.scss';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
