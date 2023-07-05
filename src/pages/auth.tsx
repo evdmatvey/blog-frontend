be964073efa597a146d1adf712bb412aff773c75
NyTakoeSebe <evd.matvey@mail.ru>
2023-07-05 11:47:27 +0700

    fixed some problems with unused imports and user's type path

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,5 +1,4 @@
 import { AuthCard } from '@/widgets/AuthCard';
-import Container from '@/shared/ui/Container';
 import getServerSideProps from '@/shared/utils/getProps/getProps';
 
 import styles from '@/app/styles/Auth.module.scss';
